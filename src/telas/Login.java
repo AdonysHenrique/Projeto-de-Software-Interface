23080f1e25cc24f0cfa6f97daa9ec542925e9749
Adonys Henrique <adonyshenriquevs@gmail.com>
2023-03-22 02:11:13 -0300

    first

---
+++ src/telas/Login.java
@@ -0,0 +1,43 @@
+package telas;
+
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+
+public class Login {
+    
+    @FXML
+    TextField login;
+    @FXML
+    PasswordField senha;
+    @FXML
+    Label mensagem;
+
+    @FXML
+    private void validacao(ActionEvent Joel){
+
+       
+        if(login.getText().equals("admin") && senha.getText().equals("admin")){
+           System.out.println("Validado!");
+           mensagem.setTextFill(Color.rgb(100, 240, 40));
+           mensagem.setText("Validado!");
+
+            
+        }
+        else{
+            mensagem.setTextFill(Color.rgb(255 , 0, 0));
+            mensagem.setText("Dados incorretos");
+            JOptionPane.showMessageDialog(null, "dados incorretos");
+
+        }
+
+    }
+    
+
+}
